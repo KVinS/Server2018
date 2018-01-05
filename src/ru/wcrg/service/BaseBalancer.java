237cf2254f0fd768067b46fd8a791d0f1ca7efb4
KVinS <kvins-java@yandex.ru>
2018-01-05 17:29:37 +0300

    Набросок работы балансера

--- src/ru/wcrg/service/BaseBalancer.java
+++ src/ru/wcrg/service/BaseBalancer.java
@@ -1,9 +1,14 @@
 package ru.wcrg.service;
 
+import ru.wcrg.Logger;
 import ru.wcrg.ThreadSettings;
 import ru.wcrg.messaging.Abonent;
 import ru.wcrg.messaging.Address;
 import ru.wcrg.messaging.MessageSystem;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 
 /**
  * Created by Эдуард on 05.01.2018.
@@ -11,16 +16,26 @@ import ru.wcrg.messaging.MessageSystem;
 public class BaseBalancer implements Abonent, Runnable {
     protected final Address address = new Address();
     protected final MessageSystem messageSystem;
+    protected final HashMap<BaseService, Integer> servicesDuration;
 
     public BaseBalancer(MessageSystem messageSystem){
         this.messageSystem = messageSystem;
         messageSystem.addAbonent(this);
+        servicesDuration = new HashMap<>();
     }
 
     @Override
     public void run() {
         while (true) {
             messageSystem.execForAbonent(this);
+
+            for(Map.Entry<BaseService, Integer> entry : servicesDuration.entrySet()) {
+                int average = entry.getValue();
+                if (average > ThreadSettings.SERVICE_SLEEP_TIME){
+                    BaseService service = entry.getKey();
+                    Logger.LogError("Need new service for " + service);
+                }
+            }
 
             try {
                 Thread.sleep(ThreadSettings.BALANCER_SLEEP_TIME);
@@ -33,5 +48,19 @@ public class BaseBalancer implements Abonent, Runnable {
     @Override
     public Address getAddress() {
         return address;
+    }
+
+    public void reportDurationWork(BaseService baseService, int duration){
+        int average = servicesDuration.get(baseService);
+        int newAverage = (average + duration) / 2;
+        servicesDuration.put(baseService, newAverage);
+    }
+
+    public void add(BaseService baseService){
+        servicesDuration.put(baseService, 0);
+    }
+
+    public void remove(BaseService baseService){
+        servicesDuration.remove(baseService);
     }
 }
